97ceb84e1742eb1951482dc2b513d699e9127933
Steven Nguyen <steven.nguyen@traveloka.com>
2020-02-27 19:28:14 +0800

    file download react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Helpers from './helpers.js';
+class App extends Component {
+  state = {
+    loading: false, // to keep track of when form submitted
+    errors: null, // for displaying errors
+    file: '', // the file type the user chooses to download
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSubmit = (event) => {
+    this.setState({
+      errors: null,
+      loading: true,
+    }, () => {
+      Helpers.httpRequest(
+        `http://localhost:5001?file=${this.state.file}`,
+        'get',
+      )
+      // 1. Convert the data into 'blob'
+      .then((response) => {
+        response.blob();
+        debugger
+      })
+      .then((blob) => {
+      
+        // 2. Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `sample.${this.state.file}`);
+        // 3. Append to html page
+        document.body.appendChild(link);
+        // 4. Force download
+        link.click();
+        // 5. Clean up and remove the link
+        link.parentNode.removeChild(link);
+        this.setState({
+          loading: false
+        });
+      })
+      .catch((error) => {
+        error.json().then((json) => {
+          this.setState({
+            errors: json,
+            loading: false
+          });
+        })
+      });
+    });
+    
+    event.preventDefault();
+  } 
+
+  handleChange = (event) => {
+    this.setState({
+      // substring to is to limit to 3 characters
+      file: event.currentTarget.value.substring(0, 3)
+    });
+  }
+
+  render() {
+    const { file, loading, errors } = this.state;
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col col-12">
+              <header className="App-header">
+                <h1 className="display-4 mt-4 mb-4">React File Downloader</h1>
+                <p>This is to demonstrate the ability to download a file via an API request and interpret that file to automatically download on the client side.</p>
+              </header>
+               
+              <main> 
+              <form onSubmit={this.handleSubmit}>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">sample.</span>
+                  </div>
+                  <input disabled={loading} onChange={this.handleChange} className="form-control" value={file} type="text" name="file" placeholder="File type, ex csv, pdf, png, etc" autoComplete="off" />
+                </div>
+                {(errors)
+                  ? (<div className="form-group">
+                      <div className="alert alert-danger"><strong>Error!</strong> {errors.message || 'Something went wrong.'}</div>
+                    </div>
+                  )
+                  : null
+                }
+                <div className="form-group">
+                  <button disabled={loading} className="btn btn-primary">{(loading) ? 'Downloading...' : 'Download'}</button>
+                </div>
+              </form>
+              </main>
+ 
+            </div>
+          </div>
+        </div>
+      </div>  
+    );
+  }
 }
-
 export default App;
